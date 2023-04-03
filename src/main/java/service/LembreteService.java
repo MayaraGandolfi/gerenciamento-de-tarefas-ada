00467d7342d76c66dccf8d27dcd92447858c1c57
MayaraGandolfi <mayaraagandolfi@gmail.com>
2023-04-02 21:39:55 -0300

    ajuste de services de Lembrete e Tarefa

--- src/main/java/service/LembreteService.java
+++ src/main/java/service/LembreteService.java
@@ -1,4 +1,27 @@
 package service;
 
+import model.Tarefa;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LembreteService {
+    private final List<Tarefa> lembretes = new ArrayList<>();
+
+    public void adicionarLembrete(Tarefa tarefa){
+        lembretes.add(tarefa);
+    }
+
+    public void removerLembrete(Tarefa tarefa){
+        lembretes.remove(tarefa);
+    }
+
+    public List<Tarefa> listarTodosLembretes(){
+        return lembretes;
+    }
+
+    public int quantidadeDeLembretes(){
+        return lembretes.size();
+    }
 }
--- src/main/java/service/TarefaService.java
+++ src/main/java/service/TarefaService.java
@@ -1,4 +1,26 @@
 package service;
 
+import model.Tarefa;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
 public class TarefaService {
+
+    public double calcularTempo(Tarefa tarefa, LocalDate hoje) throws IllegalArgumentException {
+        if (tarefa.isConcluida()) {
+            throw new IllegalArgumentException("A tarefa já está concluida!");
+        }
+        return Duration.between(tarefa.getDataCriacao().atStartOfDay(), hoje.atStartOfDay()).toDays();
+    }
+
+    public void concluirTarefa(Tarefa tarefa) {
+        tarefa.setConcluida(true);
+     }
+
+     public void alterarLembrete(Tarefa tarefa,boolean statusLembrete){
+           tarefa.setPossuiLembrete(statusLembrete);
+     }
+
+
 }
